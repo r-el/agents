6948264af5d84f539344d161a081d52ef47bebc0
Ariel <arieltanami122@gmail.com>
2025-06-08 16:48:18 +0300

    Implement AgentDal class for agent data access operations

---
+++ DAL/AgentDal.cs
@@ -0,0 +1,24 @@
+namespace Agents
+{
+      public class AgentDal : AgentDalBase
+      {
+            public override void AddAgent(Agent agent)
+            {
+
+            }
+
+            public override List<Agent> GetAllAgents()
+            {
+                  return [];
+            }
+
+            public override void UpdateAgentLocation(int agentId, string newLocation)
+            {
+
+            }
+            public override void DeleteAgent(int agentId)
+            {
+
+            }
+      }
+}
